c04833bb62419bc8a5f8dc8b336225a6770df981
Agustin Camino <agustin.camino@gmail.com>
2015-12-02 07:27:17 -0500

    Add IVR welcome
    * Add the java helper library

---
+++ src/main/java/com/twilio/phonetree/IVRWelcomeServlet.java
@@ -0,0 +1,42 @@
+package com.twilio.phonetree;
+
+import com.twilio.sdk.verbs.Gather;
+import com.twilio.sdk.verbs.Play;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IVRWelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
+            throws IOException {
+
+        Gather gather = new Gather();
+        gather.setAction("/");
+        gather.setNumDigits(1);
+
+        Play play = new Play("http://howtodocs.s3.amazonaws.com/et-phone.mp3");
+        play.setLoop(3);
+
+        try {
+            gather.append(play);
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+
+        TwiMLResponse twiMLResponse = new TwiMLResponse();
+        try {
+            twiMLResponse.append(gather);
+        } catch (TwiMLException e) {
+            e.printStackTrace();
+        }
+
+        servletResponse.setContentType("text/xml");
+        servletResponse.getWriter().write(twiMLResponse.toXML());
+    }
+}
--- src/test/java/com/twilio/phonetree/IVRWelcomeServletTest.java
+++ src/test/java/com/twilio/phonetree/IVRWelcomeServletTest.java
@@ -7,22 +7,21 @@ import org.mockito.MockitoAnnotations;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
 
-public class ExampleServletTest {
+public class IVRWelcomeServletTest {
 
     @Mock
     HttpServletRequest request;
 
     @Mock
     HttpServletResponse response;
-
-    @Mock
-    PrintWriter printWriter;
 
     @Before
     public void setUp() {
@@ -32,11 +31,15 @@ public class ExampleServletTest {
     @Test
     public void testDoGet() throws Exception {
 
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintWriter printWriter = new PrintWriter(output);
         when(response.getWriter()).thenReturn(printWriter);
-        ExampleServlet servlet = new ExampleServlet();
 
+        IVRWelcomeServlet servlet = new IVRWelcomeServlet();
         servlet.doGet(request, response);
 
-        verify(printWriter).write(anyString());
+        printWriter.flush();
+        String responseContent = new String(output.toByteArray(), "UTF-8");
+        assertThat(responseContent, is(not("")));
     }
 }
