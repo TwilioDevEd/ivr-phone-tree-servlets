8298ed3e11a11e12566d00b1575c13496e23cc02
Agustin Camino <agustin.camino@gmail.com>
2015-12-02 14:28:45 -0500

    Add connect servlet

---
+++ src/main/java/com/twilio/phonetree/servlet/commuter/ConnectServlet.java
@@ -0,0 +1,47 @@
+package com.twilio.phonetree.servlet.commuter;
+
+import com.twilio.sdk.verbs.Dial;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConnectServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse)
+            throws IOException {
+
+        String selectedOption = servletRequest.getParameter("digits");
+        Map<String, String> optionPhones = new HashMap<>();
+        optionPhones.put("2", "+12024173378");
+        optionPhones.put("3", "+12027336386");
+        optionPhones.put("4", "+12027336637");
+
+        TwiMLResponse twiMLResponse = null;
+
+        if (optionPhones.containsKey(selectedOption)) {
+            try {
+                twiMLResponse = dial(optionPhones.get(selectedOption));
+            } catch (TwiMLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        servletResponse.setContentType("text/xml");
+        servletResponse.getWriter().write(twiMLResponse.toXML());
+    }
+
+    private TwiMLResponse dial(String phoneNumber) throws TwiMLException {
+
+        TwiMLResponse response = new TwiMLResponse();
+        response.append(new Dial(phoneNumber));
+
+        return response;
+    }
+}
---
+++ src/test/java/com/twilio/phonetree/servlet/commuter/ConnectServletTest.java
@@ -0,0 +1,62 @@
+package com.twilio.phonetree.servlet.commuter;
+
+import com.twilio.phonetree.TwilioServletTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.hamcrest.CoreMatchers;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(JUnitParamsRunner.class)
+public class ConnectServletTest extends TwilioServletTest {
+
+    @Mock HttpServletRequest request;
+
+    @Mock HttpServletResponse response;
+
+    ByteArrayOutputStream output;
+
+    PrintWriter printWriter;
+
+    @Before
+    public void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+
+        output = new ByteArrayOutputStream();
+        printWriter = new PrintWriter(output);
+        when(response.getWriter()).thenReturn(printWriter);
+    }
+
+    @Test
+    @Parameters({"2", "3", "4"})
+    public void whenSelectedOptionIs_2_3_4_ThenResponseContainsDial(String digits) throws IOException, JDOMException {
+
+        when(request.getParameter("digits")).thenReturn(digits);
+
+        ConnectServlet servlet = new ConnectServlet();
+        servlet.doPost(request, response);
+
+        printWriter.flush();
+        String content = new String(output.toByteArray(), "UTF-8");
+
+        Document document = getDocument(content);
+
+        assertThatContentTypeIsXML(response);
+        assertThat(getElement(document, "Dial").getValue(), is(CoreMatchers.<String>notNullValue()));
+    }
+}
